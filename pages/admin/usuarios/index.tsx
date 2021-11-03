b4e76b42b905d5fedc22b118f1bb74ac61e0dc47
Jose Angarita <amjose09@gmail.com>
2021-11-02 21:28:38 -0500

    Agregar link para volver al home desde admin/usuarios

--- pages/admin/usuarios/index.tsx
+++ pages/admin/usuarios/index.tsx
@@ -1,7 +1,14 @@
+import Link from 'next/link'
+
 const index = () => {
     return (
         <div>
             Pagina de admin de usuarios
+            <Link href="/">
+                <a className="bg-red-400 px-5 py-2 rounded-lg">
+                    Home
+                </a>
+            </Link>
         </div>
     )
 }
